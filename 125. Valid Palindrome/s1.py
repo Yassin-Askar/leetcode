ba618fca0acf1d95b8c060e3b7a3b665d2ab4366
Yassin Askar <yassinaskar.cs@gmail.com>
2022-07-06 19:34:47 +0200

    update-125. Valid Palindrome-s1.py

--- 125. Valid Palindrome/s1.py
+++ 125. Valid Palindrome/s1.py
@@ -2,5 +2,4 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(filter(str.isalnum, s))
         s = s.lower()
-        print(f"s :{s} : {s[::-1]}")
         return s == s[::-1]
