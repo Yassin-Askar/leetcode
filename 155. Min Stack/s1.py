7dec188437ffc88342276ea51abd56fff2a1bbce
Yassin Askar <yassinaskar.cs@gmail.com>
2022-07-13 18:27:58 +0200

    add--155. Min Stack--s1.py

--- 155. Min Stack/s1.py
+++ 155. Min Stack/s1.py
@@ -1,27 +1,24 @@
-from inspect import stack
-
-
 class MinStack:
-    min_num:int
-    stack:list
 
     def __init__(self):
-        min_num =None
-        stack = []
+        self.min_num = float('inf')
+        self.stack = []
         pass
 
     def push(self, val: int) -> None:
-        stack.append(val)
+        if val < self.min_num:
+            self.min_num = val
+        self.stack.append([val, self.min_num])
 
     def pop(self) -> None:
+        self.stack.pop()
+        if self.stack:
+            self.min_num = self.stack[-1][1]
+        else:
+            self.min_num = float('inf')
 
     def top(self) -> int:
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-
-
-obj = MinStack()
-obj.push(val)
-obj.pop()
-param_3 = obj.top()
-param_4 = obj.getMin()
+        return self.min_num
