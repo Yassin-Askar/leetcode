d4e5a5567bf2b6aa20cf195adb63f982780379dc
Yassin Askar <yassinaskar.cs@gmail.com>
2022-07-21 06:22:12 +0200

    add-875. Koko Eating Bananas-s1.pt

---
+++ 875. Koko Eating Bananas/s1.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        hight = max(piles)
+        mid = 0
+        low = 1
+        k = mid
+
+        while low <= hight:
+            mid = (hight + low)//2
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile/mid)
+
+            if hours <= h:
+                k = mid
+                hight = mid - 1
+            else:
+                low = mid + 1
+
+        return k
+
+
+piles = [30, 11, 23, 4, 20]
+h = 6
+print(Solution().minEatingSpeed(piles=piles, h=h))
