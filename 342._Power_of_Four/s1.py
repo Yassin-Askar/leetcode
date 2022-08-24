f54bf0efa2a6e00fdacf4ef80e8cd10d012aae36
Yassin Askar <yassinaskar.cs@gmail.com>
2022-08-24 23:19:56 +0200

    add-342. Power of Four-s1.py

---
+++ 342._Power_of_Four/s1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n > 1:
+            n /= 4
+        return n == 1
+
+
+n = 4
+print(Solution(). isPowerOfFour(n=n, ))
