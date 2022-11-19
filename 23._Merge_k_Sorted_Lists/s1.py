481949edc90f0e64078eaa48203a33c5fbccbbf7
Yassin Askar <yassinaskar.cs@gmail.com>
2022-11-19 17:50:47 +0200

    add-23. Merge k Sorted Lists-s1.py

---
+++ 23._Merge_k_Sorted_Lists/s1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists or len(lists) == 0:
+            return None
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeList(self, list1, list2):
+        cur = dummy = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+
+        return dummy.next
+
+
+lists = list[Optional[listNode]]
+print(Solution(). mergeKlists(lists=lists, ))
