96473f55fc45f1232f741f959305c8b6f7a8b396
Yassin Askar <yassinaskar.cs@gmail.com>
2023-02-19 18:00:35 +0200

    add-295. Find Median from Data Stream-s1.py

---
+++ 295._Find_Median_from_Data_Stream/s1.py
@@ -0,0 +1,27 @@
+
+import heapq as hq
+
+
+class MedianFinder:
+    first_part: list
+    second_part: list
+
+    def __init__(self):
+        self.first_part = []
+        self.second_part = []
+
+    def addNum(self, num: int) -> None:
+        hq.heappush(self.first_part, -num)
+        hq.heappush(self.second_part, -hq.heappop(self.first_part))
+        if len(self.first_part) < len(self.second_part):
+            hq.heappush(self.first_part, -hq.heappop(self.second_part))
+        print(
+            f"first_part: {self.first_part}, second_part: {self.second_part}")
+
+    def findMedian(self) -> float:
+        if len(self.first_part) == len(self.second_part):
+            return (-self.first_part[0] + (self.second_part[0])) / 2
+        return self.second_part[0]
+
+
+arr = MedianFinder()
