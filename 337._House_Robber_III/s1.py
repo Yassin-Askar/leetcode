e4bf55e896a046e595624691ddcda8220425a89a
Yassin Askar <yassinaskar.cs@gmail.com>
2023-03-11 20:28:33 +0200

    add-337. House Robber III-s1.py

---
+++ 337._House_Robber_III/s1.py
@@ -0,0 +1,20 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def dfs(root):
+            if not root:
+                return [0, 0]
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            with_root = root.val + left[1] + right[1]
+            no_root = max(left) + max(right)
+            return [with_root, no_root]
+        return max(dfs(root))
