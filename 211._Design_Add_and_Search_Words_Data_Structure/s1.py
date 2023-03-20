604e8d849d3643597dc6187c69979f7ecd7f8f89
Yassin Askar <yassinaskar.cs@gmail.com>
2023-03-21 00:05:40 +0200

    add-211. Design Add and Search Words-s1.py

--- 211._Design_Add_and_Search_Words_Data_Structure/s1.py
+++ 211._Design_Add_and_Search_Words_Data_Structure/s1.py
@@ -1,3 +1,4 @@
+
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -5,7 +6,6 @@ class TrieNode:
 
 
 class WordDictionary:
-
     def __init__(self):
         self.root = TrieNode()
 
@@ -14,21 +14,24 @@ class WordDictionary:
         for c in word:
             if c not in curr.children:
                 curr.children[c] = TrieNode()
-                curr = curr.children[c]
+            curr = curr.children[c]
         curr.word = True
 
     def search(self, word: str) -> bool:
         def dfs(j, root):
             curr = root
+
             for i in range(j, len(word)):
                 c = word[i]
                 if c == ".":
                     for child in curr.children.values():
-                        dfs(i+1, child)
-                        return True
+                        if dfs(i + 1, child):
+                            return True
+                    return False
                 else:
                     if c not in curr.children:
                         return False
                     curr = curr.children[c]
             return curr.word
+
         return dfs(0, self.root)
