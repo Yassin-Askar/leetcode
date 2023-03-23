16f19fb2fb3164a3a190e945874ea3c96c14dfef
Yassin Askar <yassinaskar.cs@gmail.com>
2023-03-23 17:35:28 +0200

    11 line

--- 75._Sort_Colors/s1.py
+++ 75._Sort_Colors/s1.py
@@ -1,12 +1,8 @@
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
-        number_map = {0: 0, 1: 0, 2: 0}
+        i, number_map = 0, {0: 0, 1: 0, 2: 0}
         for i in range(len(nums)):
-            if nums[i] not in number_map:
-                number_map[nums[i]] = 1
-            else:
-                number_map[nums[i]] += 1
-        i = 0
+            number_map[nums[i]] += 1
         for k, v in number_map.items():
             val = v
             while val > 0:
