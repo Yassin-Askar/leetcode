ffe749b3fd8201352f87d9cf8ad3c19df26e928c
Yassin Askar <yassinaskar.cs@gmail.com>
2023-03-28 05:38:16 +0200

    update-88. Merge Sorted Array-s1.py

--- 88._Merge_Sorted_Array/s1.py
+++ 88._Merge_Sorted_Array/s1.py
@@ -3,10 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        while len(nums1) > m:
-            nums1.pop()
-        nums1 += nums2
-        nums1.sort()
+        p1 = m - 1
+        p2 = n - 1
+        x = 0
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            x += 1
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
 
 
 nums1 = [1, 2, 3, 0, 0, 0]
