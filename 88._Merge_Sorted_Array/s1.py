a9966a8e69dd2d8b13aea71b7990326e6b1a195f
Yassin Askar <yassinaskar.cs@gmail.com>
2023-03-28 05:40:09 +0200

    update

--- 88._Merge_Sorted_Array/s1.py
+++ 88._Merge_Sorted_Array/s1.py
@@ -5,11 +5,11 @@ class Solution:
         """
         p1 = m - 1
         p2 = n - 1
-        x = 0
+
         for p in range(n + m - 1, -1, -1):
             if p2 < 0:
                 break
-            x += 1
+
             if p1 >= 0 and nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
                 p1 -= 1
