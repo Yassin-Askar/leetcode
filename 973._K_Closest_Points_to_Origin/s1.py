387dd8928711f5010be743e9d6433b25f846cad0
Yassin Askar <yassinaskar.cs@gmail.com>
2023-03-28 20:52:33 +0200

    add-973. K Closest Points to Origin-s1.py

---
+++ 973._K_Closest_Points_to_Origin/s1.py
@@ -0,0 +1,30 @@
+from heapq import *
+
+
+class Solution:
+	def kClosest(self, points: list[list[int]], k: int):
+
+		maxHeap =[]
+		origin = [0,0]
+		point = points.pop()
+		distant  = ((origin[0]-point[0])**2+(origin[1]-point[1])**2)
+		maxHeap = [[-distant,point[0],point[1]]]
+		res = []
+		heapify(maxHeap)
+
+		while points:
+			point = points.pop()
+			distant  = ((origin[0]-point[0])**2+(origin[1]-point[1])**2)
+			heappush(maxHeap,[-distant,point[0],point[1]])
+			if len(maxHeap) > k :
+				heappop(maxHeap)
+
+		for _,x,y in maxHeap:
+			res.append([x,y])
+		return res
+
+
+
+points =[[3,3],[5,-1],[-2,4]]
+k = 2
+print(Solution(). kClosest(points = points , k = k , ))
